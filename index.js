c6a992f6616e9ccc2cf2f0bbce37374beb80396d
Chris N <anull@proton.me>
2023-05-24 13:21:09 +0200

    feat: added varsIgnorePattern to facilitate key deletion via object serialization.

--- index.js
+++ index.js
@@ -43,6 +43,7 @@ module.exports = {
     "@typescript-eslint/consistent-type-definitions": ["error", "type"],
     "@typescript-eslint/consistent-type-imports": "error",
     "@typescript-eslint/no-import-type-side-effects": "error",
+    "@typescript-eslint/no-unused-vars": ["error", { "varsIgnorePattern": "^_" }],
     "@typescript-eslint/strict-boolean-expressions": [
       "error",
       {
